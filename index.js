0a06279998e0affbd631f153cdb7d0602d1dd1b0
Ahmed1ad <96233908+Ahmed1ad@users.noreply.github.com>
2025-12-30 13:37:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { Pool } = require('pg');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const app = express();
